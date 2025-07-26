1f5feae29d993ca0cc6bdf241fae523f36e77e88
Max <imax.mr@gmail.com>
2025-07-25 22:39:41 -0700

    indent fix

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -4,18 +4,18 @@ export function saveTokens ({ data }) {
 };
 
 export const convertToTimezone = (datestring) => {
-        const date = new Date(datestring);
+    const date = new Date(datestring);
 
-        const formatted = new Intl.DateTimeFormat('en-US', {
-            weekday: 'short',
-            year: 'numeric',
-            month: 'short',
-            day: '2-digit',
-            hour: 'numeric',
-            minute: '2-digit',
-            timeZoneName: 'short',
-            hour12: true,
-            }).format(date);
+    const formatted = new Intl.DateTimeFormat('en-US', {
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit',
+        hour: 'numeric',
+        minute: '2-digit',
+        timeZoneName: 'short',
+        hour12: true,
+        }).format(date);
 
-        return formatted;
-    }
+    return formatted;
+}
